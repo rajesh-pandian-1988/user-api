5d69e0aa6ecac98d9c42315b9ee1ffd7c661e11d
Rajesh Pandian <rajesh_pandian@outlook.com>
2023-06-25 08:46:41 +0100

    update terraform resource

--- main.tf
+++ main.tf
@@ -17,6 +17,8 @@ resource "azurerm_linux_web_app" "terraform_app_service" {
   resource_group_name = azurerm_resource_group.terraform_app_service_rg.name
   service_plan_id     = azurerm_service_plan.terraform_app_service_plan.id
   site_config {
+    always_on         = false
+    use_32_bit_worker = true
     application_stack {
       dotnet_version = "6.0"
     }
